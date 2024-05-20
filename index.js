d6e47ab668e99340106dd5812da249855965ba3d
nahel-b <nahelbelma@gmail.com>
2024-05-20 23:38:55 +0200

    fix spotifycallback

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+
 const apiRoutes = require('./api');
 require('dotenv').config();
 const { connectToDatabase } = require('./database');
@@ -37,30 +38,26 @@ app.get('/spotifycallback', async (req, res) => {
   }
 
   try {
-    console.log("code",code);
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + Buffer.from(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64')
-      },
-      body: queryString.stringify({
+    request.post({
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
         grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: REDIRECT_URI
-      })
-    });
-
-    const data = await response.json();
-    console.log("data",data);
-    const access_token = data.access_token;
-    console.log("access_token",access_token);
-    if (access_token) {
+        code,
+        redirect_uri: REDIRECT_URI,
+        client_id: SPOTIFY_CLIENT_ID,
+        client_secret: SPOTIFY_CLIENT_SECRET,
+      },
+    }, (error, response, body) => {
+      if (error) {
+        console.error('Error getting Spotify token:', error);
+        return res.status(500).send('Internal Server Error');
+      }
+      
+      const { access_token } = JSON.parse(body);
+      
       // Redirect back to the mobile app with the token
       res.redirect(`${MOBILE_APP_REDIRECT_URI}?access_token=${access_token}`);
-    } else {
-      res.status(500).send('Failed to retrieve access token');
-    }
+    });
   } catch (error) {
     console.error('Error getting Spotify token:', error);
     res.status(500).send('Internal Server Error');
